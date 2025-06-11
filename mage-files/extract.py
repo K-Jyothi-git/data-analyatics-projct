1dbc874159206089534e8ee121e8bc143e2f861b
K Jyothi <kalakotijyothi01@gmail.com>
2025-06-11 11:10:00 +0530

    added python file

--- mage-files/extract.py
+++ mage-files/extract.py
@@ -7,4 +7,15 @@ if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
 
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'https://storage.googleapis.com/uber-data-engineering-project-darshil/uber_data.csv'
+    response = requests.get(url)
+
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
 
