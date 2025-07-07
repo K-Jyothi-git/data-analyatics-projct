4c28d59dca0f812b4d2fd9e6a23ed25eccc53138
K Jyothi <kalakotijyothi01@gmail.com>
2025-07-07 11:10:00 +0530

    updated python file

---
+++ mage-files/load.py
@@ -0,0 +1,6 @@
+from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from pandas import DataFrame
+from os import path
+
