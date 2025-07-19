77d66a31297db31ec3f274c581d7b024a70a42f0
K Jyothi <kalakotijyothi01@gmail.com>
2025-07-19 11:10:00 +0530

    updated python file

--- mage-files/load.py
+++ mage-files/load.py
@@ -3,3 +3,9 @@ from mage_ai.io.bigquery import BigQuery
 from mage_ai.io.config import ConfigFileLoader
 from pandas import DataFrame
 from os import path
+
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
