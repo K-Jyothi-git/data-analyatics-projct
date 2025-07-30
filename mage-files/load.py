cdc49b81cd93f7a7cbb95ac691dea472c7008705
K Jyothi <kalakotijyothi01@gmail.com>
2025-07-30 11:10:00 +0530

    updated python file

--- mage-files/load.py
+++ mage-files/load.py
@@ -9,3 +9,23 @@ if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 
+@data_exporter
+def export_data_to_big_query(data, **kwargs) -> None:
+    """
+    Template for exporting data to a BigQuery warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#bigquery
+
+    
+    """
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    for key, value in data.items():
+        table_id = 'data-with-darshil.uber_data_engineering_yt.{}'.format(key)
+        BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            DataFrame(value),
+            table_id,
+            if_exists='replace',  # Specify resolution policy if table name already exists
+        )
