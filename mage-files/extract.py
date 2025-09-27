6254c0a05c5e6b84a922a1140eecfd88ec52b6e3
K Jyothi <kalakotijyothi01@gmail.com>
2025-09-27 11:10:00 +0530

    updated python file

---
+++ mage-files/extract.py
@@ -0,0 +1,13 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+
+
+
+    
