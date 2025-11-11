c90d2adc2ec44dc81a2b188433304cdf52ff5fcc
K Jyothi <kalakotijyothi01@gmail.com>
2025-11-11 11:10:00 +0530

    updated python file

---
+++ mage-files/transform.py
@@ -0,0 +1,8 @@
+import pandas as pd
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+
